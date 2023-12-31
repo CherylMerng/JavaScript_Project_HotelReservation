9e2161e63d3f0288813b7c50d8fcf890b17ca740
CherylMerng <nskmerng@gmail.com>
2023-12-31 20:18:36 +0630

    javascript practice

--- app.js
+++ app.js
@@ -1,61 +1,46 @@
-let bookBtn = document.getElementById("bookBtn");
-let resetBtn = document.getElementById("resetBtn");
-//v0 // let result = document.getElementById("result");
-
 let prices = [120, 80, 40];
 let roomTypes = ["Sea View", "Second Sea View", "Garden View"];
+let bookBtnValue = document.getElementById("bookBtn");
 
-resetBtn.onclick = function() {
-    customerName.value = "";
-    fromDate.value = "";
-    toDate.value = "";
-    roomType.value = "";
-    totalRoom.value = "";
-}
-
-bookBtn.onclick = function() {
-    let name = document.getElementById("customerName").value;
-    let fDate = document.getElementById("fromDate").value;
-    let tDate = document.getElementById("toDate").value;
-    let typeIndex = document.getElementById("roomType").value;
-    let type = roomTypes[typeIndex - 1];
-    let room = document.getElementById("totalRoom").value;
- 
+bookBtnValue.onclick = function() {
+    let customerNameValue = document.getElementById("customerName").value;
+    let dateInValue = document.getElementById("dateIn").value;
+    let dateOutValue = document.getElementById("dateOut").value;
+    let daysValue = document.getElementById("days").value;
+    let roomTypeValue = document.getElementById("roomType").value;
+    let roomType = roomTypes[roomTypeValue - 1];
+    let roomsValue = document.getElementById("rooms").value;
+    
     let price = 0;
 
-    if (typeIndex == 1) {
-        price = prices[0] * room;
+    if (roomTypeValue == 1) {
+        price = prices[0] * daysValue * roomsValue;
     }
-    else if (typeIndex == 2) {
-        price = prices[1] * room;
+    else if (roomTypeValue == 2) {
+        price = prices[1] * daysValue * roomsValue;
     }
-    else if (typeIndex == 3) {
-        price = prices[2] * room;
+    else if (roomTypeValue == 3) {
+        price = prices[2] * daysValue * roomsValue;
     }
 
-    //v0 // result.innerHTML = name + " " + type + " " + room + " " + price;
-    displayQuotation(name, fDate, tDate, type, room, price);
+    displayQuotation(customerNameValue, dateInValue, dateOutValue, roomType, roomsValue, price);
 }
 
-function displayQuotation(name, fdate, tdate, type, room, price) {
+function displayQuotation(name, dateIn, dateOut, type, room, price) {
     let quoteTable = document.getElementById("quoteTable");
     let row = quoteTable.insertRow(-1);
 
     let nameCell = row.insertCell(0);
-    let fDateCell = row.insertCell(1);
-    let tDateCell = row.insertCell(2);
+    let dateInCell = row.insertCell(1);
+    let dateOutCell = row.insertCell(2);
     let typeCell = row.insertCell(3);
     let roomCell = row.insertCell(4);
     let priceCell = row.insertCell(5);
 
     nameCell.textContent = name;
-    fDateCell.textContent = fdate;
-    tDateCell.textContent = tdate;
+    dateInCell.textContent = dateIn;
+    dateOutCell.textContent = dateOut;
     typeCell.textContent = type;
     roomCell.textContent = room;
     priceCell.textContent = "$" + price.toFixed(2);
 }
-
-// durationInMilliseconds = toDate - fromDate;
-// durationInDays = durationInMilliseconds / (1000 * 60 * 60 * 24)
-    // 1000milliseconds/second × 60seconds/minute × 60minutes/hour × 24hours/day
